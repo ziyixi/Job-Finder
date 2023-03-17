f303ea5d0f9125158c46678f35d713fdf69a1b0b
Ziyi Xi <ziyixi@mail.ustc.edu.cn>
2023-03-17 17:23:24 -0400

    refactor Google APP by ChatGPT

--- job_finder/job_finder/spiders/google/__init__.py
+++ job_finder/job_finder/spiders/google/__init__.py
@@ -13,6 +13,7 @@ class GoogleSpider(scrapy.Spider):
         search_url = self.settings.get("GOOGLE_SEARCH_URL")
         key_words = self.settings.get("GOOGLE_KEY_WORDS")
         urls = [f"{search_url}{key_word}" for key_word in key_words]
+
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse, meta=dict(
                 playwright=True,
@@ -20,85 +21,75 @@ class GoogleSpider(scrapy.Spider):
                     PageMethod(
                         "evaluate", "window.scrollBy(0, document.body.scrollHeight)"),
                     PageMethod("wait_for_timeout", 1000),
-                ],))
+                ],
+            ))
 
     def parse(self, response):
-        all_job_posts = response.xpath(
-            '//*[@id="search-results"]/li/div/a')
-        if len(all_job_posts) == 0:
+        all_job_posts = response.xpath('//*[@id="search-results"]/li/div/a')
+
+        if not all_job_posts:
             self.logger.info(f"No job posts found for {response.url}")
             return
 
         count = 0
-        for i, each in enumerate(all_job_posts):
+        for index, post in enumerate(all_job_posts):
             try:
-                jobitem = self.parse_job_item(each)
-                if jobitem is not None:
+                jobitem = self.parse_job_item(post)
+                if jobitem:
                     yield jobitem
                     count += 1
-                else:
-                    continue
             except:
                 self.logger.error(
-                    f"Error parsing job item {i} in {response.url}")
+                    f"Error parsing job item {index} in {response.url}")
+
         self.logger.info(f"Handled {count} job posts in {response.url}")
 
         next_page = response.xpath(
             '//a[@data-gtm-ref="search-results-next-click"]/@href').get()
-        next_page_visiable = response.xpath(
+        next_page_visible = response.xpath(
             '//a[@data-gtm-ref="search-results-next-click"]/@style').get() != "display: none;"
 
-        if (next_page is not None) and next_page_visiable:
+        if next_page and next_page_visible:
             yield response.follow(next_page, callback=self.parse, meta=dict(
                 playwright=True,
                 playwright_page_methods=[
                     PageMethod(
                         "evaluate", "window.scrollBy(0, document.body.scrollHeight)"),
                     PageMethod("wait_for_timeout", 1000),
-                ],))
+                ],
+            ))
 
     def parse_job_item(self, post_item: Selector):
         title = post_item.xpath(
             './/h2[@itemprop="title"]/text()').get().strip()
-        for exclude in self.settings.get("GOOGLE_EXCLUDE_KEY_WORDS"):
-            if title.lower().find(exclude.lower()) != -1:
-                return None
-        city_parsed = post_item.xpath(
-            './/span[@itemprop="addressLocality"]/text()')
-        if city_parsed:
-            city = city_parsed.get().strip()
-        else:
-            city = ""
-        country_parsed = post_item.xpath(
-            './/span[@itemprop="addressCountry"]/text()')
-        if country_parsed:
-            country = country_parsed.get().strip()
-        else:
-            country = ""
+        exclude_keywords = self.settings.get("GOOGLE_EXCLUDE_KEY_WORDS")
+
+        if any(exclude.lower() in title.lower() for exclude in exclude_keywords):
+            return None
+
+        city = post_item.xpath('.//span[@itemprop="addressLocality"]/text()').get().strip(
+        ) if post_item.xpath('.//span[@itemprop="addressLocality"]/text()') else ""
+        country = post_item.xpath('.//span[@itemprop="addressCountry"]/text()').get(
+        ).strip() if post_item.xpath('.//span[@itemprop="addressCountry"]/text()') else ""
 
         date = post_item.xpath(
             './/meta[@itemprop="datePosted"]/@content').get().strip()
         description = post_item.xpath(
             './/meta[@itemprop="description"]/@content').get().strip()
         relative_url = post_item.xpath('.//@href').get()
-        # relative_url: https://careers.google.com/jobs/results/113538880541467334-student-researcher-phd-2023/?distance=50&q=phd
-        # unique_id: 113538880541467334
+
         unique_id = post_item.xpath('.//@href').re_first(r'(\d+)-')
 
         key_words = self.settings.get("GOOGLE_KEY_WORDS", [])
-        flag_should_return_None = True
-        for key_word in key_words:
-            if key_word.lower() in title.lower() or key_word.lower() in description.lower():
-                flag_should_return_None = False
-                break
-        if flag_should_return_None:
+
+        if not any(keyword.lower() in title.lower() or keyword.lower() in description.lower() for keyword in key_words):
             return None
 
         item = JobItem(
             id=unique_id,
             title=title,
             company=self.name,
-            location=city+country,
+            location=f"{city}{country}",
             description=description,
             url=self.settings.get("GOOGLE_BASE_URL") + relative_url,
             date=parser.parse(date),
